1b7a900a040e34d83ae6d9ccb874ad77e690f29c
Esteban Munoz <emunozh@gmail.com>
2016-02-12 10:05:18 +0100

    reduce toc on pdf

--- doc/guidelines/build/makelatex.sh
+++ doc/guidelines/build/makelatex.sh
@@ -5,5 +5,8 @@ pandoc -S -o Guidelines_EnergyADE.tex \
     --filter pandoc-citeproc \
     Guidelines_EnergyADE.md metadata.yaml
 
+sed -i 's/\\textbackslash{}degree\\{C\\}/\\degree{C}/' Guidelines_EnergyADE.tex
+
 pdflatex Guidelines_EnergyADE.tex
 mv Guidelines_EnergyADE.pdf ../Guidelines_EnergyADE.pdf
+
