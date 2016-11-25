610af3f8df70131752a3f2e5ff35f75827bb4ce6
pascal-schetelat <pascal.schetelat@cstb.fr>
2016-11-25 20:11:45 +0100

    Add requirement for lxml and pytest Added travis.yml ci file for github

--- tests/test_schematron_validation.py
+++ tests/test_schematron_validation.py
@@ -1,27 +1,25 @@
-
+from lxml import isoschematron
 from lxml import etree
 import os
 from glob import glob
 import pytest
 
-
-xsd_path = os.path.join('..','xsd','energy.xsd')
-sample_directory  = os.path.join('..','samples')
+schematron_path = os.path.join('..','schematron','energy.xsd')
+sample_directory = os.path.join('..','samples')
 citygml_files = glob(sample_directory+os.path.sep+'*.gml')
 
 
+# need an internet connection to resolve the imported schemas
+def test_simple_validation() :
 
-def test_samples_validation() :
+    with open(schematron_path) as f :
+        schematron_doc = etree.parse(f)
 
-
-    with open(xsd_path) as f :
-        xmlschema_doc = etree.parse(f)
-
-    xmlschema = etree.XMLSchema(xmlschema_doc)
+    schematron = isoschematron.Schematron(schematron_doc)
 
     for files in citygml_files :
         with open(files) as gml_f:
             gml_obj = etree.parse(gml_f)
 
-        print xmlschema.validate(gml_obj)
+        assert schematron.validate(gml_obj)
 
