6dda9c5b16fc21e7ccc2383a4805b70ac72cfd91
pascal-schetelat <pascal.schetelat@cstb.fr>
2016-11-26 11:58:39 +0100

    Add error log details

--- tests/test_xsd_validation.py
+++ tests/test_xsd_validation.py
@@ -14,11 +14,16 @@ with open(xsd_path) as f :
 xmlschema = etree.XMLSchema(xmlschema_doc)
 
 #XMLSchemaParseError -> need an internet connection to resolve the imported schemas
-@pytest.mark.parametrize("file_path,expected", [(i, None) for i in citygml_files])
+@pytest.mark.parametrize("file_path,expected", [(i, os.path.basename(i)) for i in citygml_files])
 def test_simple_validation(file_path, expected):
-
+    print "-------"
+    print file_path
     with open(file_path) as gml_f:
         gml_obj = etree.parse(gml_f)
-
-    assert xmlschema.validate(gml_obj)
-
+    if not xmlschema.validate(gml_obj):
+        print "KO"
+        print xmlschema.error_log
+        assert False
+    else:
+        print 'OK'
+        assert True
